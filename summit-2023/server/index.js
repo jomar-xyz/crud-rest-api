38d3cb5215889db8ccf2eac068f001dc969ecf0b
Jomar Bandol <jomar.bandol@boom.ai>
2023-02-18 00:25:47 +0800

    setup endpoints

--- summit-2023/server/index.js
+++ summit-2023/server/index.js
@@ -4,30 +4,41 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors())
+app.use(express.json({ limit: "500mb", extended: true }));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
+app.use(cors());
+app.use(bodyParser.json({ limit: "500mb", extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "500mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hi from server!");
 });
 
-axios.post(`http://gpu1.ailan:1111/generate_email`,{ email: "dadlskjf", initial: "initial" }).then(res=>{
-    console.log('res', res.body)
-    // res.status(200).json({
-    //   ...res.data
-    // });
-  }).catch(err => {
-    console.log('err', err)
-  })
+// axios.post(`https://fulfillment-api-t4clbpuvxa-ue.a.run.app/generate_email`,JSON.parse({ email: "dadlskjf", initial: "initial" })).then(res=>{
+//     console.log('res', res.body)
+//     // res.status(200).json({
+//     //   ...res.data
+//     // });
+//   }).catch(err => {
+//     console.log('err', err)
+//   })
 
 app.post("/", (req, res) => {
-  axios.post(`http://gpu1.ailan:1111/generate_email`,{
-    ...req.body
-  }).then(res=>{
-    res.status(200).json({
-      ...res.data
+  console.log(req.body);
+  axios
+    .post(`https://fulfillment-api-t4clbpuvxa-ue.a.run.app/generate_email`, {
+      ...req.body,
+    })
+    .then((result) => {
+      console.log(result)
+      res.status(200).json(result.data.response);
     });
-  })
 });
 
 app.listen(8080, () => {
